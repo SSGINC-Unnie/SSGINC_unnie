ddcce1b7a53ef6a4e6e907fe4b1b927b4cd2148f
DHKim96 <history8549@gmail.com>
2025-02-19 10:14:27 +0900

    feat. 마이바티스로 설정 변경

---
+++ src/main/java/com/ssginc/unnie/board/vo/BoardCategory.java
@@ -0,0 +1,21 @@
+package com.ssginc.unnie.board.vo;
+
+import lombok.Getter;
+
+@Getter
+public enum BoardCategory {
+    NOTICE("공지 있어!"),
+    SALE("여기 할인어때?"),
+    WHAT_SUITS_ME("나 뭐가 어울려?"),
+    HEARD_THIS_TIP("이 꿀팁 들어봤어?"),
+    HOWS_THIS_ITEM("이템 어때?"),
+    FREE_BOARD("자유게시판");
+
+    private final String description;
+
+    BoardCategory(String description) {
+        this.description = description;
+    }
+
+}
+
