81d869c4275a2cdfc82e9828c466c5a49363e282
LeeGaYoung <lky10047364@gmail.com>
2025-02-19 16:10:23 +0900

    feat. 회원 관련 파일 생성

---
+++ src/main/java/com/ssginc/unnie/member/controller/MemberController.java
@@ -0,0 +1,4 @@
+package com.ssginc.unnie.member.controller;
+
+public class MemberController {
+}
---
+++ src/main/java/com/ssginc/unnie/member/service/serviceImpl/MemberServiceImpl.java
@@ -0,0 +1,6 @@
+package com.ssginc.unnie.member.service.serviceImpl;
+
+import com.ssginc.unnie.member.service.MemberService;
+
+public class MemberServiceImpl implements MemberService {
+}
