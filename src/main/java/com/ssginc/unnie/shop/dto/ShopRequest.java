cf6e662ffbef804881f2c3c7253b08111017b6b2
DESKTOP-Q64LJKP\이상우 <tkdguq0654@naver.com>
2025-02-20 12:45:23 +0900

    fix. shopDTO 수정

--- src/main/java/com/ssginc/unnie/shop/dto/ShopRequest.java
+++ src/main/java/com/ssginc/unnie/shop/dto/ShopRequest.java
@@ -1,12 +1,5 @@
 package com.ssginc.unnie.shop.dto;
 
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 public class ShopRequest {
 }
