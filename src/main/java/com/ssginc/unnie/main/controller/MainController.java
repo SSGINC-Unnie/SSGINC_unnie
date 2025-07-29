7167f7bcf91d3a97b660405348195432bbe85fdb
DESKTOP-Q64LJKP\이상우 <tkdguq0654@naver.com>
2025-07-29 15:07:09 +0900

    feat. 메인페이지 이번주 꿀팁 구현

---
+++ src/main/java/com/ssginc/unnie/main/controller/MainController.java
@@ -0,0 +1,32 @@
+package com.ssginc.unnie.main.controller;
+
+import com.ssginc.unnie.common.util.ResponseDto;
+import com.ssginc.unnie.main.service.serviceImpl.MainServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+public class MainController {
+
+    private final MainServiceImpl mainService;
+
+    @Autowired
+    public MainController(MainServiceImpl mainService) {
+        this.mainService = mainService;
+    }
+
+    @GetMapping("/fetchVideos")
+    public ResponseEntity<ResponseDto<Map<String, Object>>> fetchYouTubeVideos(@RequestParam String query) {
+        String youtubeResult = mainService.getYouTubeVideos(query);
+
+        Map<String, Object> responseData = Map.of("youtubeResult", youtubeResult);
+
+        return ResponseEntity.ok(new ResponseDto<>(HttpStatus.OK.value(), "파일 조회에 성공했습니다.", responseData));
+    }
+}
---
+++ src/main/java/com/ssginc/unnie/main/service/serviceImpl/MainServiceImpl.java
@@ -0,0 +1,38 @@
+package com.ssginc.unnie.main.service.serviceImpl;
+
+import com.ssginc.unnie.main.service.MainService;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
+public class MainServiceImpl implements MainService {
+
+    @Value("${youtube.api.key}")
+    private String apiKey;
+
+
+    private final String youtubeApiUrl = "https://www.googleapis.com/youtube/v3/search";
+
+    // RestTemplate을 서비스에서 직접 생성
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    // YouTube API를 호출하여 결과 가져오기
+    public String getYouTubeVideos(String query) {
+        // URL 인코딩을 고려한 URL 구성
+        String url = UriComponentsBuilder.fromHttpUrl(youtubeApiUrl)
+                .queryParam("part", "snippet")
+                .queryParam("q", query)
+                .queryParam("type", "video")
+                .queryParam("maxResults", "4")
+                .queryParam("key", apiKey)
+                .toUriString();
+
+        // API 호출
+        String response = restTemplate.getForObject(url, String.class);
+        return response;
+    }
+}
