a53c38b3d527cdbc5fceb294ca747c7ca4df5688
DESKTOP-Q64LJKP\이상우 <tkdguq0654@naver.com>
2025-08-07 17:29:34 +0900

    fix. GeoCoding 오류 수정중

--- src/main/java/com/ssginc/unnie/shop/service/ServiceImpl/ShopServiceImpl.java
+++ src/main/java/com/ssginc/unnie/shop/service/ServiceImpl/ShopServiceImpl.java
@@ -50,7 +50,7 @@ public class ShopServiceImpl implements ShopService {
     // Haversine 공식
     @Override
     public double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
-        final int R = 6371; // 지구 반지름 (km)
+        final int R = 6371;
         double dLat = Math.toRadians(lat2 - lat1);
         double dLon = Math.toRadians(lon2 - lon1);
         double a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
