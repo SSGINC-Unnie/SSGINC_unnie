927d818bce036c43a5ce10ef8e75235a5eb7dd40
DESKTOP-Q64LJKP\이상우 <tkdguq0654@naver.com>
2025-09-30 16:14:52 +0900

    feat. 메인페이지 스케줄링과 redis 캐싱을 활용한 api 검색량 감소 구현, 계절별 검색어 변경 구현

--- src/main/java/com/ssginc/unnie/main/service/serviceImpl/MainServiceImpl.java
+++ src/main/java/com/ssginc/unnie/main/service/serviceImpl/MainServiceImpl.java
@@ -121,7 +121,6 @@ public class MainServiceImpl implements MainService {
                     .queryParam("key", apiKey)
                     .queryParam("relevanceLanguage", "ko")
                     .queryParam("regionCode", "KR")
-                    .queryParam("videoDuration", "medium")
                     .buildAndExpand(query).toUri();
 
             return restTemplate.getForObject(uri, String.class);
