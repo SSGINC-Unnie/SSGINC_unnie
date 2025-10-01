eed9e04472318b29db71954331275b95ff1b3d53
DESKTOP-Q64LJKP\이상우 <tkdguq0654@naver.com>
2025-10-01 18:38:01 +0900

    feat. 커뮤니티 신고구현

--- src/main/java/com/ssginc/unnie/report/controller/ReportController.java
+++ src/main/java/com/ssginc/unnie/report/controller/ReportController.java
@@ -1,5 +1,6 @@
 package com.ssginc.unnie.report.controller;
 
+import com.ssginc.unnie.common.config.MemberPrincipal;
 import com.ssginc.unnie.common.util.ResponseDto;
 import com.ssginc.unnie.report.dto.ReportRequest;
 import com.ssginc.unnie.report.service.ReportService;
@@ -7,7 +8,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,11 +28,11 @@ public class ReportController {
     private final ReportService reportService;
 
     @PostMapping("")
-    public ResponseEntity<ResponseDto<Map<String, Object>>> createReport(ReportRequest report) {
+    public ResponseEntity<ResponseDto<Map<String, Object>>> createReport(
+            @RequestBody ReportRequest report,
+            @AuthenticationPrincipal MemberPrincipal memberPrincipal) {
 
-        long memberId = 1;
-
-        report.setReportMemberId(memberId);
+        report.setReportMemberId(memberPrincipal.getMemberId());
 
         return ResponseEntity.ok(
                 new ResponseDto<>(HttpStatus.CREATED.value(), "신고가 접수되었습니다.", Map.of("reportId", reportService.createReport(report)))
